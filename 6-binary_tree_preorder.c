3f7ff355c8105a4ece6d2cc15c0aff72aaf07b8a
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-08-03 22:03:02 +0300

    fixessss

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,18 +2,16 @@
 
 /**
 * binary_tree_preorder - Goes through a binary tree using pre-order traversal.
+* (LDR) D => read/processing current node data
 * @tree: A pointer to the root node of the tree to traverse.
 * @func: A pointer to a function to call for each node.
-* 
-* Description: If tree or func is NULL, do nothing.
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-if (tree == NULL || func == NULL)
+if (tree && func)
 {
-return;
-}
 func(tree->n);
 binary_tree_preorder(tree->left, func);
 binary_tree_preorder(tree->right, func);
+}
 }
